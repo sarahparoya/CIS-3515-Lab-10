1380c68a3c54f090bee840d5371e35fc0f11d465
sarahparoya <sarahparoya@gmail.com>
2020-12-07 02:13:57 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/abrowser/BrowserActivity.java
@@ -0,0 +1,249 @@
+package edu.temple.abrowser;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import edu.temple.abrowser.bookmarks.Bookmark;
+import edu.temple.abrowser.bookmarks.BookmarkList;
+import edu.temple.abrowser.bookmarks.BookmarksActivity;
+
+public class BrowserActivity extends AppCompatActivity implements PageControlFragment.PageControlInterface, PageViewerFragment.PageViewerInterface, BrowserControlFragment.BrowserControlInterface, PagerFragment.PagerInterface, PageListFragment.PageListInterface {
+
+    private final int BOOKMARKS_REQUEST_CODE = 1;
+
+    FragmentManager fm;
+
+    private final String PAGES_KEY = "pages";
+
+    PageControlFragment pageControlFragment;
+    BrowserControlFragment browserControlFragment;
+    PageListFragment pageListFragment;
+    PagerFragment pagerFragment;
+
+    ArrayList<PageViewerFragment> pages;
+
+    boolean listMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null)
+            pages = (ArrayList) savedInstanceState.getSerializable(PAGES_KEY);
+        else
+            pages = new ArrayList<>();
+
+        fm = getSupportFragmentManager();
+
+        listMode = findViewById(R.id.page_list) != null;
+
+        Fragment tmpFragment;
+
+        // If PageControlFragment already added (activity restarted) then hold reference
+        // otherwise add new fragment. Only one instance of fragment is ever present
+        if ((tmpFragment = fm.findFragmentById(R.id.page_control)) instanceof PageControlFragment)
+            pageControlFragment = (PageControlFragment) tmpFragment;
+        else {
+            pageControlFragment = new PageControlFragment();
+            fm.beginTransaction()
+                    .add(R.id.page_control, pageControlFragment)
+                    .commit();
+        }
+
+        // If BrowserFragment already added (activity restarted) then hold reference
+        // otherwise add new fragment. Only one instance of fragment is ever present
+        if ((tmpFragment = fm.findFragmentById(R.id.browser_control)) instanceof BrowserControlFragment)
+            browserControlFragment = (BrowserControlFragment) tmpFragment;
+        else {
+            browserControlFragment = new BrowserControlFragment();
+            fm.beginTransaction()
+                    .add(R.id.browser_control, browserControlFragment)
+                    .commit();
+        }
+
+        // If PagerFragment already added (activity restarted) then hold reference
+        // otherwise add new fragment. Only one instance of fragment is ever present
+        if ((tmpFragment = fm.findFragmentById(R.id.page_viewer)) instanceof PagerFragment)
+            pagerFragment = (PagerFragment) tmpFragment;
+        else {
+            pagerFragment = PagerFragment.newInstance(pages);
+            fm.beginTransaction()
+                    .add(R.id.page_viewer, pagerFragment)
+                    .commit();
+        }
+
+
+        // If fragment already added (activity restarted) then hold reference
+        // otherwise add new fragment IF container available. Only one instance
+        // of fragment is ever present
+        if (listMode) {
+            if ((tmpFragment = fm.findFragmentById(R.id.page_list)) instanceof PageListFragment)
+                pageListFragment = (PageListFragment) tmpFragment;
+            else {
+                pageListFragment = PageListFragment.newInstance(pages);
+                fm.beginTransaction()
+                        .add(R.id.page_list, pageListFragment)
+                        .commit();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * Clear the url bar and activity title
+     */
+    private void clearIdentifiers() {
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setTitle("");
+        pageControlFragment.updateUrl("");
+    }
+
+    // Notify all observers of collections
+    private void notifyWebsitesChanged() {
+        pagerFragment.notifyWebsitesChanged();
+        if (listMode)
+            pageListFragment.notifyWebsitesChanged();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save list of open pages for activity restart
+        outState.putSerializable(PAGES_KEY, pages);
+    }
+
+    /**
+     * Update WebPage whenever PageControlFragment sends new Url
+     * Create new page first if none exists
+     * Alternatively, you can create an empty page when the activity first loads
+     * @param url to load
+     */
+    @Override
+    public void go(String url) {
+        if (pages.size() > 0)
+            pagerFragment.go(url);
+        else {
+            pages.add(PageViewerFragment.newInstance(url));
+            notifyWebsitesChanged();
+            pagerFragment.showPage(pages.size() - 1);
+        }
+
+    }
+
+    /**
+     * Go back to previous page when user presses Back in PageControlFragment
+     */
+    @Override
+    public void back() {
+        pagerFragment.back();
+    }
+
+    /**
+     * Go forward to next page when user presses Forward in PageControlFragment
+     */
+    @Override
+    public void forward() {
+        pagerFragment.forward();
+    }
+
+    /**
+     * Update displayed Url in PageControlFragment when Webpage Url changes
+     * only if it is the currently displayed page, and not another page
+     * @param url to display
+     */
+    @Override
+    public void updateUrl(String url) {
+        if (url != null && url.equals(pagerFragment.getCurrentUrl())) {
+            pageControlFragment.updateUrl(url);
+
+            // Update the ListView in the PageListFragment - results in updated titles
+            notifyWebsitesChanged();
+        }
+    }
+
+    /**
+     * Update displayed page title in activity when Webpage Url changes
+     * only if it is the currently displayed page, and not another page
+     * @param title to display
+     */
+    @Override
+    public void updateTitle(String title) {
+        if (title != null && title.equals(pagerFragment.getCurrentTitle()) && getSupportActionBar() != null)
+            getSupportActionBar().setTitle(title);
+
+        // Results in the ListView in PageListFragment being updated
+        notifyWebsitesChanged();
+    }
+
+    /**
+     * Add a new page/fragment to the list and display it
+     */
+    @Override
+    public void newPage() {
+        // Add page to list
+        pages.add(new PageViewerFragment());
+        // Update all necessary views
+        notifyWebsitesChanged();
+        // Display the newly created page
+        pagerFragment.showPage(pages.size() - 1);
+        // Clear the displayed URL in PageControlFragment and title in the activity
+        clearIdentifiers();
+    }
+
+    /**
+     * Add page to bookmarks
+     */
+    @Override
+    public void newBookmark() {
+        String title = pagerFragment.getCurrentTitle(),
+                url = pagerFragment.getCurrentUrl();
+        if (!title.isEmpty() && !url.isEmpty()) {
+            BookmarkList bookmarks = BookmarkList.getInstance(this);
+            bookmarks.addBookmark(new Bookmark(pagerFragment.getCurrentTitle(), pagerFragment.getCurrentUrl()));
+            bookmarks.save(this);
+            Toast.makeText(this, getString(R.string.bookmark_saved_message), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Show bookmarks dialog
+     */
+    @Override
+    public void showBookmarks() {
+        startActivityForResult(new Intent(this, BookmarksActivity.class), BOOKMARKS_REQUEST_CODE);
+    }
+
+    /**
+     * Display requested page in the PagerFragment
+     * @param position of page to display
+     */
+    @Override
+    public void pageSelected(int position) {
+        pagerFragment.showPage(position);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Open URL selected from Bookmark
+        if (requestCode == BOOKMARKS_REQUEST_CODE && resultCode == RESULT_OK && data != null)
+            go(((Bookmark) data.getSerializableExtra(BookmarksActivity.BOOKMARK_KEY)).getUrl());
+    }
+}
---
+++ app/src/main/java/edu/temple/abrowser/BrowserControlFragment.java
@@ -0,0 +1,62 @@
+package edu.temple.abrowser;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+public class BrowserControlFragment extends Fragment {
+
+    private BrowserControlInterface browserActivity;
+
+    public BrowserControlFragment() {}
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if (context instanceof BrowserControlInterface) {
+            browserActivity = (BrowserControlInterface) context;
+        } else {
+            throw new RuntimeException("Cannot manage windows if interface not BrowserControlInterface implemented");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View l = inflater.inflate(R.layout.fragment_browser_control, container, false);
+        final ImageButton newPageButton = l.findViewById(R.id.newPageButton);
+        final ImageButton newBookmarkButton = l.findViewById(R.id.newBookmark);
+        final ImageButton showBookmarksButton = l.findViewById(R.id.showBookmarks);
+
+        View.OnClickListener ocl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.equals(newPageButton))
+                    browserActivity.newPage();
+                else if (v.equals(newBookmarkButton))
+                    browserActivity.newBookmark();
+                else if (v.equals(showBookmarksButton))
+                    browserActivity.showBookmarks();
+            }
+        };
+        newPageButton.setOnClickListener(ocl);
+        newBookmarkButton.setOnClickListener(ocl);
+        showBookmarksButton.setOnClickListener(ocl);
+        return l;
+    }
+
+    interface BrowserControlInterface {
+        void newPage();
+        void newBookmark();
+        void showBookmarks();
+    }
+}
